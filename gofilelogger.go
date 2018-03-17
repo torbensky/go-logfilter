460cc391a12003eea20f013c837214fbd1cef5cd
Torben Werner <torbinsky+ghtorbensky@gmail.com>
2018-03-17 13:19:42 -0700

    TW: Added a file lookup cache.

--- gofilelogger.go
+++ gofilelogger.go
@@ -9,12 +9,26 @@ import (
 )
 
 var loggerConfig = sync.Map{} // thread safe, more performant than using regular map with mutex, I think
+var fileCache = sync.Map{}
+var UseCache = true
 
 /*
 	Sets a log level for a path
 */
 func SetLevel(level logrus.Level, path string) {
 	loggerConfig.Store(path, level)
+
+	if !UseCache {
+		return
+	}
+
+	// Bust the cache
+	fileCache.Range(func(f, v interface{}) bool {
+		if strings.Contains(f.(string), path) {
+			fileCache.Delete(f)
+		}
+		return true
+	})
 }
 
 /*
@@ -22,7 +36,7 @@ func SetLevel(level logrus.Level, path string) {
 */
 func SetLevels(level logrus.Level, paths ...string) {
 	for _, p := range paths {
-		loggerConfig.Store(p, level)
+		SetLevel(level, p)
 	}
 }
 
@@ -33,26 +47,35 @@ func SetLevels(level logrus.Level, paths ...string) {
 
 	When multiple matches are possible, this will behave non-deterministically. This is based on the underlying map behavior.
 */
-func GetFileLevel(file string) (logrus.Level, bool) {
-	dir := filepath.Dir(file)
+func GetFileLevel(file string) logrus.Level {
+	// Check if we already know the level for this file
+	cached, ok := fileCache.Load(file)
+	if ok {
+		return cached.(logrus.Level)
+	}
+
 	level := logrus.InfoLevel // default level if we don't find a match
-	found := false
+
+	dir := filepath.Dir(file)
 	loggerConfig.Range(func(k, v interface{}) bool {
 		// If we match a file exactly, immediately return
 		if strings.HasSuffix(file, k.(string)) {
 			level = v.(logrus.Level)
-			found = true
 			return false
 		}
 
 		// If we find a directory match, just remember it in case we don't find a file match
 		if strings.HasSuffix(dir, k.(string)) {
 			level = v.(logrus.Level)
-			found = true
 		}
 
 		return true
 	})
 
-	return level, found
+	// Cache this result for faster lookup next time
+	if UseCache {
+		fileCache.Store(file, level)
+	}
+
+	return level
 }
--- gofilelogger_test.go
+++ gofilelogger_test.go
@@ -12,34 +12,29 @@ func TestSetLevel(t *testing.T) {
 	_, filename, _, _ := runtime.Caller(0)
 
 	// Ensure the default level is working
-	l, found := GetFileLevel(filename)
-	assert.False(t, found)
+	l := GetFileLevel(filename)
 	assert.Equal(t, l, logrus.InfoLevel)
 
 	// Ensure the level can be overridden for a package
 	SetLevel(logrus.WarnLevel, "github.com/torbensky/gofilelogger")
-	l, found = GetFileLevel(filename)
-	assert.True(t, found, "package level")
+	l = GetFileLevel(filename)
 	assert.Equal(t, logrus.WarnLevel, l, "package level")
 
 	// Ensure the level can be overridden for a file
 	SetLevel(logrus.DebugLevel, "github.com/torbensky/gofilelogger/gofilelogger_test.go")
-	l, found = GetFileLevel(filename)
-	assert.True(t, found, "file level")
+	l = GetFileLevel(filename)
 	assert.Equal(t, logrus.DebugLevel, l, "file level")
 
 	// Try a mapping that doesn't use test filename
 	SetLevel(logrus.PanicLevel, "baz.go")
-	l, found = GetFileLevel("/foo/go/path/bar/baz/baz.go")
-	assert.True(t, found, "file level")
+	l = GetFileLevel("/foo/go/path/bar/baz/baz.go")
 	assert.Equal(t, logrus.PanicLevel, l, "file level")
 
 	// Try setting multiple paths at once
 	paths := []string{"/unicorn/foo.go", "/dragon/bar.go", "/example/foo"}
 	SetLevels(logrus.DebugLevel, paths...)
 	for _, p := range paths {
-		l, found = GetFileLevel(p)
-		assert.True(t, found, "multi-match")
+		l = GetFileLevel(p)
 		assert.Equal(t, logrus.DebugLevel, l, "multi-match")
 	}
 }
